23ef589e4f5516207353f88e09dd7530ffe7ae28
gustavotitow <135654396+gustavotitow@users.noreply.github.com>
2024-07-27 16:11:21 -0300

    Update baralho.h

--- baralho.h
+++ baralho.h
@@ -4,28 +4,37 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <raylib.h>
 
 #define NOME_NAIPE 10
+#define TAMANHO_NOME_IMAGEM 30
 
-typedef struct{
+typedef struct
+{
     int chave;
     int valor;
     char naipe[NOME_NAIPE];
     int sentido; //de costas (0) ou de frente (1);
-    Texture2D
-}Carta;
+
+    Texture2D Imagem_carta;
+
+
+} Carta;
 
 typedef Carta Info;
 
-typedef struct nodoLEnc{
+typedef struct nodoLEnc
+{
     Info info;
     struct nodoLEnc *prox;
     struct nodoLEnc *ant;
-}NodoLEnc;
+} NodoLEnc;
 
-typedef struct{
+typedef struct
+{
     struct nodoLEnc *prim;
-}ListaCircEnc;
+    struct nodoLEnc *fim;
+} ListaCircEnc;
 
 // Funcao que cria uma lista
 ListaCircEnc* criaListaCircEnc();
@@ -33,18 +42,15 @@ ListaCircEnc* criaListaCircEnc();
 // Funcao que destroi uma lista
 void destroiListaCircEnc(ListaCircEnc *lista);
 
-// Funcao que imprime todos os nodos de uma lista
-void imprimeListaCircEnc(ListaCircEnc *lista);
-
 // Funcao que insere um nodo no inicio de uma lista
 int insereInicioListaCircEnc(ListaCircEnc *lista, Info info);
 
-// Função que insere um nodo no fim de uma lista
-void insereFimListaCircEnc(ListaCircEnc *lista, Info info);
+// FunÃ§Ã£o que insere um nodo no fim de uma lista
+int insereFimListaCircEnc(ListaCircEnc *lista, Info info);
 
-// Funcao que resgata um nodo com uma informacao de uma lista
-NodoLEnc* buscaInfoListaCircEnc(ListaCircEnc* lista, int chave);
+void carrega_imagem_cartas(ListaCircEnc* baralho);
 
+ListaCircEnc* cria_baralho();
 
 
 
