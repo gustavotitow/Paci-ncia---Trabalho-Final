ce5abb78b72f0b0636382cfea4197997f896c596
gustavotitow <135654396+gustavotitow@users.noreply.github.com>
2024-07-27 16:08:30 -0300

    Update Programa.c

--- Programa.c
+++ Programa.c
@@ -1,35 +1,43 @@
 #include "baralho.h"
-#include "raylib.h"
+#include <raylib.h>
+
 
 
 int main(void)
 {
-
+    ListaCircEnc *baralho;
+    NodoLEnc *carta;
     const int screenWidth = 1000;
     const int screenHeight = 650;
+
     InitWindow(screenWidth, screenHeight, "Jogo Paciencia - trabalho final");
 
-    //Informações referentes as imagens das cartas
-    Image carta = LoadImage("cartas/2_of_clubs.png");
-    ImageResize(&carta, (carta.width - carta.width * 0.8), (carta.height - carta.height * 0.8));
-    Texture2D imagem = LoadTextureFromImage(carta);
-    UnloadImage(carta);
-
+    baralho = cria_baralho();
+    carta = baralho->prim;
     SetTargetFPS(60);
+
     while (!WindowShouldClose())
     {
         BeginDrawing();
-
         ClearBackground(RAYWHITE);
 
 
-        DrawTexture(imagem, screenWidth/2 - imagem.width/2, screenHeight/2 - imagem.height/2, WHITE);
+        int posX = 50;
+        int posY = 50;
 
-        DrawText("Jogo Paciencia", 190, 200, 20, RED);
+
+        do
+        {
+            DrawTexture(carta->info.Imagem_carta, posX, posY, WHITE);
+            carta = carta->prox;
+            posX = posX+15;
+            posY = posY+10;
+        }
+        while(carta != baralho->fim);
+
 
         EndDrawing();
     }
-
 
     CloseWindow();
 
